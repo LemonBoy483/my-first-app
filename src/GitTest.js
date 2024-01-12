c108047c459e095e95b4b25a2052dfe43287040e
LemonBoy483 <lemoneaterboy@gmail.com>
2024-01-12 15:34:52 +0800

    update

---
+++ src/GitTest.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GitTest = () => {
+  return (
+    <View>
+      <Text>GitTest</Text>
+    </View>
+  )
+}
+
+export default GitTest
+
+const styles = StyleSheet.create({})
